e2100753d1473371131a0a35957b9dd6b55d0ed1
neorafrontend <neorafrontend@gmail.com>
2021-05-05 13:34:32 -0500

    json file created for products

---
+++ js/data.js
@@ -0,0 +1,214 @@
+const product_data = [
+    {
+        "name" : "Sets",
+        "products" : [
+                {
+                    "name" : "NeoraFit Weight management & Wellness Set",
+                    "image_thumb" : "https://jaimeglas1.neora.com/-/media/Shared/Products/US-Product-Images/7234_US/Images/7234_US.ashx",
+                    "ss_price" : 130.00,
+                    "price" : 200.00,
+                },
+                {
+                    "name" : "Night & Day Combo",
+                    "image_thumb" : "https://mydreambiz.neora.com/-/media/Shared/Products/US-Product-Images/9122_US/Images/9122_US_md.ashx",
+                    "ss_price" : 142.00,
+                    "price" : 187.00,
+                },
+                {
+                    "name" : "Advanced Skincare Set",
+                    "image_thumb" : "https://alyturner.neora.com/-/media/Shared/Products/US-Product-Images/7130_US/Images/7130_US.ashx",
+                    "ss_price" : 250.00,
+                    "price" : 388.00,
+                },
+                {
+                    "name" : "CLeanse, Correct, Protect Set",
+                    "image_thumb" : "https://dafned.neora.com/-/media/Shared/Products/US-Product-Images/7179_US/Images/7179_US.ashx",
+                    "ss_price" : 185.00,
+                    "price" : 322.00,
+                },
+                {
+                    "name" : "Holistic Wellness Set",
+                    "image_thumb" : "https://dafned.neora.com/-/media/Shared/Products/US-Product-Images/7157_US/Images/7157_US.ashx",
+                    "ss_price" : 221.00,
+                    "price" : 366.00,
+                },
+                {
+                    "name" : "Proluxe Hair Care System",
+                    "image_thumb" : "https://dafned.neora.com/-/media/Shared/Products/US-Product-Images/7198_US/Images/7198_US.ashx",
+                    "ss_price" : 155.00,
+                    "price" : 249.00,
+                },
+                {
+                    "name" : "Night, Day & Cleanser Combo",
+                    "image_thumb" : "https://dafned.neora.com/-/media/Shared/Products/US-Product-Images/9118_US/Images/9118_US.ashx",
+                    "ss_price" : 173.00,
+                    "price" : 228.00,
+                },
+                {
+                    "name" : "Eye Serum/Eye Patch Combo",
+                    "image_thumb" : "https://dafned.neora.com/-/media/Shared/Products/US-Product-Images/9115_US/9115_US.ashx",
+                    "ss_price" : 100.00,
+                    "price" : 135.00,
+                },
+                {
+                    "name" : "Acne Pads & Cleanser Combo",
+                    "image_thumb" : "https://dafned.neora.com/-/media/Shared/Products/US-Product-Images/9132_US/Images/9132_US.ashx",
+                    "ss_price" : 75.00,
+                    "price" : 100.00,
+                },
+                {
+                    "name" : "Youth Factor Combo Pack",
+                    "image_thumb" : "https://dafned.neora.com/-/media/Shared/Products/US-Product-Images/9117_US/9117_US.ashx",
+                    "ss_price" : 94.00,
+                    "price" : 120.00,
+                },
+                {
+                    "name" : "Wellness Chews Combo",
+                    "image_thumb" : "https://sjennings0325.neora.com/-/media/Shared/Products/US-Product-Images/9129_US/Images/9129_US.ashx",
+                    "ss_price" : 80.00,
+                    "price" : 106.00,
+                },
+                {
+                    "name" : "Proluxe Shampoo/Conditioner Combo",
+                    "image_thumb" : "https://dafned.neora.com/-/media/Shared/Products/US-Product-Images/9133_US/Images/9133_US.ashx",
+                    "ss_price" : 64.00,
+                    "price" : 86.00,
+                }
+            ]
+    },
+    {
+        "name" : "Skincare",
+        "products" : [
+            {
+                "name" : "Age IQ Night Cream",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1165_US/Images/1165_US.ashx",
+                "ss_price" : 92.00,
+                "price" : 120.00,
+            },
+            {
+                "name" : "Age IQ Day Cream",
+                "image_thumb" : "https://triciaromett.neora.com/-/media/Shared/Products/US-Product-Images/1166_US/Images/1166_US.ashx",
+                "ss_price" : 77.00,
+                "price" : 100.00,
+            },
+            {
+                "name" : "Double-Cleansing Face Wash",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1163_US/Images/1163_US.ashx",
+                "ss_price" : 31.00,
+                "price" : 41.00,
+            },
+            {
+                "name" : "Eye Serum",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1156_US/Images/1156_US.ashx",
+                "ss_price" : 60.00,
+                "price" : 80.00,
+            },
+            {
+                "name" : "Invisi-Bloc SPF40",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1173_US/Images/1173_US_md.ashx",
+                "ss_price" : 44.00,
+                "price" : 59.00,
+            },
+            {
+                "name" : "IllumaBoost Vitamin C",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1162_US/Images/1162_US.ashx",
+                "ss_price" : 60.00,
+                "price" : 80.00,
+            },
+            {
+                "name" : "Firm Contour Cream",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1154_US/Images/1154_US.ashx",
+                "ss_price" : 80.00,
+                "price" : 106.00,
+            },
+            {
+                "name" : "Eye-V Hydrogel Patches",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1158_US/Images/1158_US.ashx",
+                "ss_price" : 40.00,
+                "price" : 55.00,
+            },
+            {
+                "name" : "Complexion Treatment Pads",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1172_US/Images/1172_US.ashx",
+                "ss_price" : 44.00,
+                "price" : 59.00,
+            },
+        ]
+    },
+    {
+        "name" : "Hair Care",
+        "products" : [
+            {
+                "name" : "Proluxe Shampoo",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1179_US/Images/1179_US.ashx",
+                "ss_price" : 32.00,
+                "price" : 43.00,
+            },
+            {
+                "name" : "Proluxe Conditioner",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1178_US/Images/1178_US.ashx",
+                "ss_price" : 32.00,
+                "price" : 43.00,
+            },
+            {
+                "name" : "Proluxe Hair Mask",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1180_US/Images/1180_US.ashx",
+                "ss_price" : 50.00,
+                "price" : 67.00,
+            },
+            {
+                "name" : "Proluxe Scalp Treatment",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1181_US/Images/1181_US.ashx",
+                "ss_price" : 72.00,
+                "price" : 96.00,
+            },
+        ]
+    },
+    {
+        "name" : "Wellness",
+        "products" : [
+            {
+                "name" : "NeoraFit",
+                "image_thumb" : "https://www.neora.com/-/media/Shared/Products/US-Product-Images/7234_US/Images/7234_US.ashx",
+                "ss_price" : 130.00,
+                "price" : 200.00,
+            },
+            {
+                "name" : "EHT Brain Formula",
+                "image_thumb" : "https://dhamilton.neora.com/-/media/Shared/Products/US-Product-Images/1155_US/Images/1155_US.ashx",
+                "ss_price" : 60.00,
+                "price" : 80.00,
+            },
+            {
+                "name" : "Youth Factor Vitality Complex",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1160_US/Images/1160_US.ashx",
+                "ss_price" : 47.00,
+                "price" : 60.00,
+            },
+            {
+                "name" : "Youth Factor Superfood & Antioxidant",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1161_US/Images/1161_US.ashx",
+                "ss_price" : 47.00,
+                "price" : 60.00,
+            },
+            {
+                "name" : "Prolistic Pre/Probiotic Vitamins",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1167_US/Images/1167_US.ashx",
+                "ss_price" : 47.00,
+                "price" : 60.00,
+            },
+            {
+                "name" : "Sleep+ Wellness Chews",
+                "image_thumb" : "https://kevingoodwin.neora.com/-/media/Shared/Products/US-Product-Images/1171_US/Images/1171_US.ashx",
+                "ss_price" : 40.00,
+                "price" : 53.00,
+            },
+            {
+                "name" : "Energy+ Wellness Chews",
+                "image_thumb" : "https://morosco87.neora.com/-/media/Shared/Products/US-Product-Images/1170_US/Images/1170_US.ashx",
+                "ss_price" : 40.00,
+                "price" : 53.00,
+            },
+        ]
+    }
+]
